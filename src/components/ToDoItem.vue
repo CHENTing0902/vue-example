cc56f80271c0476224198c1d98a2c5962e710c98
TING CHEN <ting@tingMacBook.elecom>
2022-04-22 11:25:27 +0900

    init TodoItem component

---
+++ src/components/ToDoItem.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <input type="checkbox" id="todo-item" :checked="done" />
+        <label for="todo-item">{{label}}</label>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        label: { require: true, type: String },
+        done: { default: false, type: Boolean},
+        id: { require: true, tyep: String }
+    },
+    data() {
+        return {
+            idDone: this.done
+        }
+    }
+};
+</script>
